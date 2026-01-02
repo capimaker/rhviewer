9eb07802c54a11a56bb2eaaffcf879fa30b2933c
Carlos Ramos <capimaker@gmail.com>
2026-01-02 18:08:46 +0100

    i create a navbar with buttons and bg

---
+++ app/(root)/components/Navbar/Navbar.data.ts
@@ -0,0 +1,6 @@
+export const links = [
+    { name: "Dashboard", href: "/dashboard" },
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "#cta" },
+    { name: "Pricing", href: "#pricing" },
+]
---
+++ app/(root)/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Logo } from "@/componentes/Shared/Logo/Logo";
+import { NavbarDesktop } from "./NavbarDesktop/NavbarDesktop";
+export  function Navbar() {
+  return (
+  <nav className="w-[90%] md:w-full md:max-w-5xl mx-auto sticky top-5 z-20 flex item-center justify-between px-6 py-3 bg-blue-300/30 backdrop-blur-md rounded-full shadow-lg border border-neutral-300/20">
+   
+  <Logo/>
+  <NavbarDesktop/>
+  </nav>
+  );
+}
