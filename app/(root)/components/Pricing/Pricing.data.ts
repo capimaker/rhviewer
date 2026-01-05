9da73fb7880aa7a4696a5f1e0acbfb80e01e323d
Carlos Ramos <capimaker@gmail.com>
2026-01-05 15:41:04 +0100

    pricing home it's done

---
+++ app/(root)/components/Pricing/Pricing.data.ts
@@ -0,0 +1,33 @@
+import { b } from "framer-motion/client";
+import { de } from "zod/locales";
+
+export const pricingPlans = [
+    {
+        name: 'Free',
+        price: '0/month',
+        description: 'Ideal for individuals starting their interview preparation journey.',
+        features: [
+            'Access to basic interview questions',
+            'Basic analytics and progress tracking',
+            'Basic coding challenges',
+            'General feedback on answers',
+        ],
+        buttonText: "Start for Free",
+        popular: false,
+    },
+    {
+        name: 'Pro',
+        price: '29/month', 
+        description: 'Perfect for professionals aiming to enhance their interview skills.',
+        features: [
+            'Access to advanced interview questions',
+            'Detailed analytics and progress tracking',     
+            'Personalized feedback on answers',
+            'Company-specific interview',
+            'Mock interview simulations',
+            'Priority support',
+        ],
+        buttonText: "Start Pro Trial",
+        popular: true,
+    },
+]
