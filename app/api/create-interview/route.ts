d32f93a6fc2639cf7f928bd02c22586bf84c6b6b
Carlos Ramos <capimaker@gmail.com>
2026-01-12 21:00:55 +0100

    fix: buggs fixed succesfuly

--- app/api/create-interview/route.ts
+++ app/api/create-interview/route.ts
@@ -1,29 +1,32 @@
-import { auth } from "@clerk/nextjs/server";
+import {  currentUser } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(req:Request) {
-    const { userId } = await auth ();
+    const user = await currentUser();
 
     try{
-        if(!userId){
+        if(!user){
             return new NextResponse("Unauthorizeed", {status: 401 });
 
         }
 
-        let user = await db.user.findUnique({ where: {id:userId}});
+        let userTmp = await db.user.findUnique({ where: {userId:user.id}});
 
-        if(!user) {
-            user = await db.user.create({
+        if(!userTmp) {
+            userTmp = await db.user.create({
                 data: {
-                    id: userId, 
+                    userId: user.id, 
+                    email: user?.emailAddresses[0]?.emailAddress,
+                    name: user?.firstName,
                 }
             })
         }
         const { name, rol , level } = await req.json();
+
         const interview = await db.interview.create({
             data: {
-                userId: userId,
+                userId: userTmp.userId,
                 name: name,
                 rol: rol,
                 level: level,
